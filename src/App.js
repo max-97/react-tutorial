8bc3a28ed4c4d3997d9af1ec6b4b622b08461d19
Max Braun <maxbraun97@web.de>
2021-10-03 13:25:13 +0200

    Lesson 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
-  return (
+    const title = "Welcome to the new Blog!"
+    const likes = 50
+    const person = {name: "yoshi", age: 30}
+    const link = "http://www.google.com"
+
+    return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+          <p>Liked {likes} times</p>
+          {/*<p>{person}</p>*/}
+          <p>{10}</p>
+          <p>{"Hello"}</p>
+          <p>{[1,2,3,4,5]}</p>
+          <p>{Math.random()*10}</p>
+          <a href={link}>Google Site</a>
       </div>
     </div>
   );
