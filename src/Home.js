0ce38ed87b1eb35f8363bbe5a57e9eab2810db56
Max Braun <maxbraun97@web.de>
2021-10-04 17:24:21 +0200

    Lesson 8

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,23 @@
+import {useState} from "react";
+
 const Home = () => {
+    //let name = "mario";
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState(25);
 
     const handleClick = (e) => {
-        console.log("Hello World!", e)
+        setName("luigi");
+        setAge(30);
     }
 
     const handleClickAgain = (name, e) => {
-        console.log("Hello " + name, e.target)
+        console.log("Hello World", e.target);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} ist {age} years old.</p>
             <button onClick={handleClick}>Click me</button>
             <button onClick={(e) => handleClickAgain("Mario", e)}>Click me again</button>
         </div>
