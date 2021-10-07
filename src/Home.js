0c17eae2145b4840142f7c51e51cd0fe5496d3be
Max Braun <maxbraun97@web.de>
2021-10-07 20:55:50 +0200

    Lesson 10

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,19 @@
 import {useState} from "react";
 
 const Home = () => {
-    //let name = "mario";
-    const [name, setName] = useState("mario");
-    const [age, setAge] = useState(25);
-
-    const handleClick = (e) => {
-        setName("luigi");
-        setAge(30);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log("Hello World", e.target);
-    }
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} ist {age} years old.</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain("Mario", e)}>Click me again</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 };
