83a0eb3056214d1f56a9b1170b0bbc143b593ff3
Max Braun <maxbraun97@web.de>
2021-10-09 12:13:09 +0200

    Lesson 13

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = (/*props*/ {blogs, title}) => {
+const BlogList = (/*props*/ {blogs, title, handleDelete}) => {
     //const blogs = props.blogs;
     //const title = props.title;
 
@@ -9,6 +9,7 @@ const BlogList = (/*props*/ {blogs, title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
