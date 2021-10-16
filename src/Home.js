76eebb0ce4d6e83f5b91c20b885d87251cb270df
Max Braun <maxbraun97@web.de>
2021-10-16 22:27:22 +0200

    Lesson 18

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,22 @@
 import {useState, useEffect} from "react";
 import BlogList from "./BlogList";
-
+// npx json-server -m src/jsonServerConf.js --watch data/db.json --port 8000
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch("blogs/").then(res => {
             return res.json();
         }).then((data) => {
             setBlogs(data);
+            setIsPending(false);
         })
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
     );
