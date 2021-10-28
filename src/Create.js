b7146ff3209aba8de311d483acb47e81762dceca
Max Braun <maxbraun97@web.de>
2021-10-28 10:24:38 +0200

    Lesson 29

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("mario");
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
-        console.log(blog);
+
+        setIsPending(true);
+
+        fetch('blogs/', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("New blog added.");
+            setIsPending(false);
+        });
     }
 
     return (
@@ -36,7 +47,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button>}
+                { isPending && <button disabled>Adding Blog...</button>}
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
